a15b5b045dcb02f30a18edced989de1618dfc3a4
KaiDrange <kaimdrange@hotmail.com>
2015-08-30 13:55:50 +0200

    Create audiofactory.h

---
+++ audiofactory.h
@@ -0,0 +1,65 @@
+#ifndef AUDIOFACTORY_H
+#define AUDIOFACTORY_H
+
+#include <QObject>
+#include <QString>
+#include <QVector>
+#include <sndfile.hh>
+#include "otwriter.h"
+
+#define	BUFFER_LENGTH	4096
+#define SILENT_SLICE_NAME "--- silent slice (1 second in length outside grid mode) ---"
+
+enum SliceMode_t { NormalMode = 0, GridMode = 1, StepsMode = 2 };
+
+class AudioFactory : public QObject
+{
+    Q_OBJECT
+
+public slots:
+    void generateFiles();
+public:
+    static QString getFormatString(const QString &file);
+    void setUISelections(const int sampleRate,
+                         const int bitRate,
+                         const int channels,
+                         const QVector<QString> sourceFiles,
+                         const QString &outFileName,
+                         const Loop_t loopSetting,
+                         const Stretch_t stretchSetting,
+                         const TrigQuant_t trigQuantSetting,
+                         const int gain,
+                         const int tempo,
+                         const int steps,
+                         const SliceMode_t sliceMode);
+
+signals:
+    void doneGenerating();
+    void fileProgress(int currentSlice, int totalSlices);
+private slots:
+private:
+    void createOutput_NormalMode();
+    void createOutput_GridMode();
+    void createOutput_StepsMode();
+
+    int buffer[BUFFER_LENGTH];
+    OTWriter *otWriter;
+    int currentFileNo;
+    uint32_t currentFileStartPoint;
+
+    int sampleRate;
+    int bitRate;
+    int channels;
+    QVector<QString> sourceFiles;
+    QString outFileName;
+    Loop_t loopSetting;
+    Stretch_t stretchSetting;
+    TrigQuant_t trigQuantSetting;
+    int gain;
+    int tempo;
+    int steps;
+    SliceMode_t sliceMode;
+};
+
+#endif // AUDIOFACTORY_H
+
